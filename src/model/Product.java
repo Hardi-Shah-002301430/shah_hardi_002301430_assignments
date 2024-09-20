8ac22fc74030ce6214a70542e7779a67de51a5e4
Hardi007 <hardishah0071@gmail.com>
2024-09-19 22:04:50 -0400

    Create Personal profile

---
+++ src/model/Product.java
@@ -0,0 +1,192 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author rohanjauhari
+ */
+public class Product {
+    String firstName;
+    String middlename;
+    String lastName;
+    String email;
+    String phone;
+    String dls;
+    String dln;
+    String ssn;
+    String al1;
+    String al2;
+    String city;
+    String state;
+    String zip;
+    String telephoneNumber;
+    String fax;
+    String hobbies;
+    String status;
+    String languages;
+    String dob;
+    String nationality;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    
+   public String getMiddleName(){
+       return middlename;
+   }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    
+    public String getDriversLicenseState() {
+        return dls;
+    }
+    
+    public String getDriversLicenseNumber() {
+        return dln;
+    }
+    
+    public String getSocialSecurityNumber() {
+        return ssn;
+    }
+    
+    public String getAddressLine1() {
+        return al1;
+    }
+    
+    public String getAddressLine2() {
+        return al2;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+    
+    public String getState() {
+        return state;
+    }
+    
+    public String getZip() {
+        return zip;
+    }
+    
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+    
+    public String getFaxNumber() {
+        return fax;   
+    }    
+    
+    public String getHobbies(){
+        return hobbies;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+    
+    public void setMiddleName(String middlename){
+        this.middlename = middlename;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setDriversLicenseState(String dls) {
+        this.dls = dls;
+    }
+    
+    public void setDriversLicenseNumber(String dln) {
+        this.dln = dln;
+    }
+    
+    public void setSocialSecurityNumber(String ssn) {
+        this.ssn = ssn;
+    }
+    
+    public void setAddressLine1(String al1) {
+        this.al1 = al1;
+    }
+    
+    public void setAddressLine2(String al2) {
+        this.al2 = al2;
+    }
+    
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+    
+    public void setFaxNumber(String fax) {
+        this.fax = fax;
+    }
+    
+    public void setHobbies(String hobbies){
+        this.hobbies = hobbies;
+    }  
+}
